a5ac249402b9c81aef1749261987287402d5e201
Pushpak-Jaiswal <pushpakmjaiswal@gmail.com>
2026-04-20 10:49:07 +0530

    final updates in security server

--- security/src/main/java/com/pushcode/security/config/SpringSecurity.java
+++ security/src/main/java/com/pushcode/security/config/SpringSecurity.java
@@ -1,9 +1,57 @@
 package com.pushcode.security.config;
 
+import com.pushcode.security.filter.JwtFilter;
+import com.pushcode.security.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    // Inject the CorsFilter bean created in CorsConfig
+    @Autowired
+    private CorsFilter corsFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/public/**", "/auth/login","/auth/register").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                // place CorsFilter before the standard UsernamePasswordAuthenticationFilter
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                // then add your JwtFilter (so it executes after CORS handling but before auth)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
